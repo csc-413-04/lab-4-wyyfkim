ec18ff018a87b76980f96746ae5a2a8226b0c9bf
Yangyifan Wang <ywang20@mail.sfsu.edu>
2018-10-25 20:53:30 -0700

    addHeader and modify app
    addHeader and modify app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
+import Header from './Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'black',
+      banner: 'hello',
+      isOpen: false,
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+    this.textHandler = this.textHandler.bind(this);
+  }
+
+  buttonHandler() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  textHandler(e) {
+    this.setState({
+      banner: e.target.value,
+    })
+  }
+
   render() {
+    let myVariable = <h2>Brian</h2>;
+    let myBanner;
+    if (this.state.isOpen) {
+      myBanner = <Header banner={this.state.banner}/>;
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h2>413 Starter Kit </h2>
+          {myVariable}
+          </p> 
+          {myBanner}     
+          {
+            this.state.isOpen && 
+            <Header banner={this.state.banner}/>
+          }
+          <input value={this.state.banner} onChange={this.textHandler}/>
+          <button onClick={this.buttonHandler} >Click Me</button>
         </header>
       </div>
     );
